289663d7f42847adf4565f334dfe0454499066c3
Michael Binder <rednib.coding@gmail.com>
2023-08-21 17:06:12 +0200

    refactored og dll call func from macro to template function

--- src/RKC_FONTMAKER/src/core.cpp
+++ src/RKC_FONTMAKER/src/core.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <cstdint>
+#include "utils.h"
 
 class RKC_FONTMAKER {
     // Based on the offsets and the sizeof(undefined4) which is 4 bytes.
@@ -17,6 +18,12 @@ public:
 
 };
 
+// Pointers to the original functions
+// These can be deleted once the DLL is entirely rewritten
+typedef VOID(WINAPI* deconstructorPtr)(RKC_FONTMAKER* self);
+typedef VOID(WINAPI* createDIBPtr)(RKC_FONTMAKER* self, HDC* hdc);
+typedef VOID(WINAPI* drawDoubleFontPtr)(RKC_FONTMAKER* self, HDC* hdc, unsigned char* param);
+
 extern "C" {
     void __thiscall RKC_FONTMAKER_constructor(RKC_FONTMAKER* self)
     {
@@ -31,12 +38,31 @@ extern "C" {
 
     void __thiscall RKC_FONTMAKER_deconstructor(RKC_FONTMAKER* self)
     {
-        self->member1 = 0;
-        self->member2 = 0;
-        self->member3 = 0;
-        self->member4 = 0;
-        self->member5 = 0;
-        self->member6 = 0;
-        self->member7 = 0;
+        LOAD_AND_EXECUTE_ORIGINAL_FUNC(
+            "o_RKC_FONTMAKER.dll",
+            deconstructorPtr,
+            "??1RKC_FONTMAKER@@QAE@XZ",
+            self
+        );
+    }
+
+    void __thiscall CreateDIB(RKC_FONTMAKER* self, HDC* hdc)
+    {
+        LOAD_AND_EXECUTE_ORIGINAL_FUNC(
+            "o_RKC_FONTMAKER.dll",
+            createDIBPtr,
+            "?CreateDIB@RKC_FONTMAKER@@QAEHPAUHDC__@@@Z",
+            self, hdc
+        );
+    }
+
+    void __thiscall DrawDoubleFont(RKC_FONTMAKER* self, HDC* hdc, unsigned char* ucharParam)
+    {
+        LOAD_AND_EXECUTE_ORIGINAL_FUNC(
+            "o_RKC_FONTMAKER.dll",
+            drawDoubleFontPtr,
+            "?DrawDoubleFont@RKC_FONTMAKER@@QAEHPAUHDC__@@PAE@Z",
+            self, hdc, ucharParam
+        );
     }
 }
